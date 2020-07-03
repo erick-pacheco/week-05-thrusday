c50bbd0840877de7231ae08b7fe098b1e8e5b66f
erick-pacheco <linarespachecoerick@gmail.com>
2020-07-02 20:16:04 -0400

    XOR() and ALL() are working

--- all.js
+++ all.js
@@ -1,6 +1,18 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
+let arr = [true, true, true]
 
+function all(arr) {
+	
+	let result = true
+	// Loop through every item in the array
+	arr.forEach(number => {
+	// If the item evaluates to false, return false
+		if (number === false) {
+			result = false
+		}
+	})
+	;
 	// If the loop ends, return true
+	return result
 }
+
+console.log(all(arr))
--- xor.js
+++ xor.js
@@ -1,7 +1,17 @@
+let cond1 = true;
+let cond2 = false;
+
+
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
-
+	if(cond1 === true &&  cond2 === false || cond2 === true &&  cond1 === false) {
+		return true
+	}
 	// if cond1 is false and cond2 is true return true
-
+	else {
+		return false
+	}
 	// return false
-}
+};
+
+console.log(xor(cond1, cond2));
